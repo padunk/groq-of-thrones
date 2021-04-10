1dd4beedd94337ab9728f257e82d5c88cc686d3d
Padunk <abraham.agel@gmail.com>
2021-04-10 14:06:02 +0700

    build: manual chunks

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,46 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import hljs from "highlight.js";
-import { plugin as md, Mode } from "vite-plugin-markdown";
-import markdownIt from "markdown-it";
-import markdownItAttrs from "markdown-it-attrs";
+import reactRefresh from "@vitejs/plugin-react-refresh"
+import hljs from "highlight.js"
+import { plugin as md, Mode } from "vite-plugin-markdown"
+import markdownIt from "markdown-it"
+import markdownItAttrs from "markdown-it-attrs"
 
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
 export default {
-    plugins: [
-        reactRefresh(),
-        md({
-            mode: [Mode.HTML, Mode.TOC, Mode.REACT],
-            markdown: (body) => {
-                console.log(`body`, body);
-            },
-            markdownIt: markdownIt({
-                linkify: true,
-                typographer: true,
-                highlight: (code, lang) => {
-                    if (lang && hljs.getLanguage(lang)) {
-                        try {
-                            return hljs.highlight(lang, code).value;
-                        } catch (__) {}
-                    }
+  plugins: [
+    reactRefresh(),
+    md({
+      mode: [Mode.HTML, Mode.TOC, Mode.REACT],
+      markdown: (body) => {
+        console.log(`body`, body)
+      },
+      markdownIt: markdownIt({
+        linkify: true,
+        typographer: true,
+        highlight: (code, lang) => {
+          if (lang && hljs.getLanguage(lang)) {
+            try {
+              return hljs.highlight(lang, code).value
+            } catch (__) {}
+          }
 
-                    return "";
-                },
-            }).use(markdownItAttrs),
-        }),
-    ],
-};
+          return ""
+        },
+      }).use(markdownItAttrs),
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          characters: ["/src/lessons/data/characters.json"],
+          quotes: ["/src/lessons/data/quotes.json"],
+          chakra: ["@chakra-ui/layout"],
+        },
+      },
+    },
+  },
+}
